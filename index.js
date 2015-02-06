1d9e755a3020fc4c7938ae62a4ec10b15eaf0b74
Takuto Wada <takuto.wada@gmail.com>
2015-02-07 00:32:18 +0900

    chore(licensify): update copyright year

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
  *
  * Copyright (c) 2014-2015 Takuto Wada
  * Licensed under the MIT license.
- *   http://twada.mit-license.org/
+ *   http://twada.mit-license.org/2014-2015
  */
 module.exports = require('./lib/licensify');
