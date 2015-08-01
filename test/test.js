1e61cf356c18de75ccfde7bb4f2a937b70db5c94
Takuto Wada <takuto.wada@gmail.com>
2015-08-01 19:49:04 +0900

    feat(licensify): add homepage field

--- test/test.js
+++ test/test.js
@@ -65,6 +65,33 @@ describe('licensify', function () {
             assert(re.test(header));
         });
     });
+
+    var expectedUrls = [
+        'homepage: https://github.com/twada/licensify',
+        'homepage: https://github.com/beatgammit/base64-js',
+        'homepage: https://github.com/feross/buffer',
+        'homepage: https://github.com/isaacs/core-util-is',
+        'homepage: https://github.com/Gozala/events',
+        'homepage: https://github.com/feross/ieee754#readme',
+        'homepage: https://github.com/isaacs/inherits',
+        'homepage: https://github.com/retrofox/is-array',
+        'homepage: https://github.com/juliangruber/isarray',
+        'homepage: https://github.com/shtylman/node-process#readme',
+        'homepage: https://github.com/calvinmetcalf/process-nextick-args',
+        'homepage: https://github.com/nodejs/readable-stream#readme',
+        'homepage: https://github.com/rvagg/string_decoder',
+        'homepage: https://github.com/rvagg/through2#readme',
+        'homepage: https://github.com/twada/type-name',
+        'homepage: https://github.com/defunctzombie/node-util',
+        'homepage: https://github.com/TooTallNate/util-deprecate',
+        'homepage: https://github.com/Raynos/xtend'
+    ];
+    expectedUrls.forEach(function (url) {
+        var re = new RegExp(' \* ' + url + '$', 'gm');
+        it('ensure header includes [' + url + ']', function () {
+            assert(re.test(header));
+        });
+    });
 });
 
 
